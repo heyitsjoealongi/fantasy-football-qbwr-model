28e8055386065ac817c018c920b2837fd7ad8951
Joe Alongi <heyitsjoealongi@outlook.com>
2023-10-28 23:56:07 -0400

    Create Pass Index

--- model.py
+++ model.py
@@ -3,3 +3,9 @@ import pandas as pd
 # Import Historical Sample Data CSV
 df = pd.read_csv("historical-sample-data.csv")
 print(df)
+
+# Create Pass Index
+hist_data_samp = df
+hist_data_samp = hist_data_samp[["Pass", "Outcome"]].set_index("Pass")
+index_range = hist_data_samp.index.min(), hist_data_samp.index.max()
+print(index_range)
