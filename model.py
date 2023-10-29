c1f99212839af52e9361d9de4699ebe7f433c669
Joe Alongi <heyitsjoealongi@outlook.com>
2023-10-29 00:22:10 -0400

    Graph Historical Data

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Import Historical Sample Data CSV
 df = pd.read_csv("historical-sample-data.csv")
@@ -9,3 +10,10 @@ hist_data_samp = df
 hist_data_samp = hist_data_samp[["Pass", "Outcome"]].set_index("Pass")
 index_range = hist_data_samp.index.min(), hist_data_samp.index.max()
 print(index_range)
+
+# Graph Historical Sample Data
+hist_data_samp.plot()
+plt.ylabel("Passes Caught")
+_ = plt.title("Fantasy Football QB/WR - Historical Data")
+plt.show()
+
